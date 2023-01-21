adc84e959d01ea007c32f380186a52236a258fca
jack kingham <wowknead@gmail.com>
2023-01-20 21:35:14 -0500

    Started outlining objects and solving structure.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![allow(non_snake_case)]
 
 use bevy::prelude::*;
-use bevy_mod_picking::{DefaultPickingPlugins, PickingCameraBundle, PickableBundle, DebugEventsPickingPlugin, PickingEvent};
+use bevy_mod_picking::{DefaultPickingPlugins, PickingCameraBundle, PickableBundle, PickingEvent, HoverEvent};
 use gui::*;
 
 mod gui;
 
 const TILE_SIZE: f32 = 22.;
+const GRID_SIZE: usize = 32;
 
 fn main() {
     App::new()
@@ -32,17 +33,18 @@ fn init(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, windows: Res<W
     let window = windows.get_primary().expect("Failed to find primary window");
     commands.spawn((Camera2dBundle::default(), PickingCameraBundle::default()));
 
+    let mut entity_grid: Vec<Vec<TileRef>> = vec![];
     //create the game state and all color tiles as children
     use bevy::math::*;
-    commands.spawn(GameState{})
-        .insert(SpriteBundle {
+    let game_state_entity = commands.spawn(SpriteBundle {
             transform: Transform::from_translation(vec3(-1.*window.width()/2. + TILE_SIZE, -1.*window.height()/2. + TILE_SIZE, 0.)),
             ..default()
         })
         .with_children(|builder| {
-            for i in 0..32 {
-                for j in 0..32 {
-                    builder.spawn(SpriteBundle {
+            for i in 0..GRID_SIZE {
+                entity_grid.push(Vec::new());
+                for j in 0..GRID_SIZE {
+                    let e = builder.spawn(SpriteBundle {
                         transform: Transform {
                             translation: vec3(i as f32*(TILE_SIZE+2.), j as f32*(TILE_SIZE+2.), 0.),
                             scale: vec3(TILE_SIZE, TILE_SIZE, 1.),
@@ -56,27 +58,51 @@ fn init(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, windows: Res<W
                     })
                     .insert(meshes.add(Mesh::from(shape::Quad::default())))
                     .insert(PickableBundle::default())
-                    .insert(GameTile);
+                    .insert(GridTile).id();
+                    entity_grid[i].push(TileRef {
+                        entity: e,
+                        position: (i, j),
+                        tile_type: TileType::None
+                    });
                 }
             }
-        });
-}
+        }).insert(GameState {
+            grid: entity_grid
+        }).id();
 
-
-#[derive(Component)]
-struct GameState {
 
 }
 
 #[derive(Component)]
-struct GameTile;
+struct GameState{
+    grid: Vec<Vec<TileRef>>
+}
 
-fn events(mut events: EventReader<PickingEvent>, mut tile_query: Query<&mut Sprite, With<GameTile>>) {
+struct TileRef {
+    entity: Entity,
+    position: (usize, usize),
+    tile_type: TileType
+}
+
+enum TileType {
+    Start, End, Wall, None
+}
+
+#[derive(Component)]
+struct GridTile;
+
+fn events(mut events: EventReader<PickingEvent>, mut tile_query: Query<&mut Sprite, With<GridTile>>, mouse: Res<Input<MouseButton>>) {
     for event in events.iter() {
         if let PickingEvent::Clicked(e) = event {
             let color: Color = tile_query.get_mut(*e).expect("Failed to find tile color").color;
             tile_query.get_mut(*e).expect("Failed to find tile color").color = if color==Color::BLACK {Color::WHITE} else {Color::BLACK};
         }
+        if let PickingEvent::Hover(HoverEvent::JustEntered(e)) = event {
+            if mouse.pressed(MouseButton::Left) {
+                tile_query.get_mut(*e).expect("Failed to find tile color").color = Color::BLACK;
+            }
+        }
+        
     }
 }
 
